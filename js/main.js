72ca50a506af5416aa0a79ccfe7eb6519a40b85f
peinorim <peinorim@gmail.com>
2014-05-19 16:45:01 +0200

    Fix ui

--- js/main.js
+++ js/main.js
@@ -21,25 +21,31 @@ $(function() {
         url: 'server/php/',
         maxFileSize: 100000,
         acceptFileTypes: /(\.|\/)(gif|jpe?g|png|bmp)$/i
+    }).bind('fileuploaddone', function(e, result) {
+        getDlFiles();
     });
 
     // Load existing files:
     $('#fileupload').addClass('fileupload-processing');
-    $.ajax({
-        // Uncomment the following to send cross-domain cookies:
-        //xhrFields: {withCredentials: true},
-        url: $('#fileupload').fileupload('option', 'url'),
-        dataType: 'json',
-        context: $('#fileupload')[0]
-    }).always(function() {
-        $(this).removeClass('fileupload-processing');
-    }).done(function(result) {
-        $(".files").html('');
-        $(this).fileupload('option', 'done')
-                .call(this, $.Event('done'), {result: result});
-        for (var key in result.files) {
-            $('#collapseOne').children().children().prepend('<label class="objet btn btn-default"><input type="radio" name="objets"><img alt="' + result.files[key].name + '" src="' + result.files[key].url + '" width="' + result.files[key].width + '" height="' + result.files[key].height + '" class="img-rounded"/></label>');
-        }
-    });
+    getDlFiles();
+
+    function getDlFiles() {
+        $.ajax({
+            // Uncomment the following to send cross-domain cookies:
+            //xhrFields: {withCredentials: true},
+            url: $('#fileupload').fileupload('option', 'url'),
+            dataType: 'json',
+            context: $('#fileupload')[0]
+        }).always(function() {
+            $(this).removeClass('fileupload-processing');
+        }).done(function(result) {
+            $(".files").html('');
+            $(this).fileupload('option', 'done')
+                    .call(this, $.Event('done'), {result: result});
+            for (var key in result.files) {
+                $('#collapseOne').children().children().prepend('<label class="objet btn btn-default"><input type="radio" name="objets"><img alt="' + result.files[key].name + '" src="' + result.files[key].url + '" width="' + result.files[key].width + '" height="' + result.files[key].height + '" class="img-rounded"/></label>');
+            }
+        });
+    }
 
 });
--- server/php/UploadHandler.php
+++ server/php/UploadHandler.php
@@ -45,7 +45,7 @@ class UploadHandler {
         $this->options = array(
             'script_url' => $this->get_full_url() . '/',
             'upload_dir' => dirname($this->get_server_var('SCRIPT_FILENAME')) . '/files/' . $_SESSION['guid'] . '/',
-            'upload_url' => $this->get_full_url() . '/' . '/files/' . $_SESSION['guid'] . '/',
+            'upload_url' => $this->get_full_url() . '/' . 'files/' . $_SESSION['guid'] . '/',
             'user_dirs' => false,
             'mkdir_mode' => 0755,
             'param_name' => 'files',
